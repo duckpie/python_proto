df23c6fd6c7b4525e97cf6c6d1d43145ee70648b
lizonn <savenkoliza1@gmail.com>
2022-03-28 20:22:11 +0300

    refresh protos

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ git clone git@github.com:wrs-news/node-protocol.git proto
 ```
 python -m grpc_tools.protoc -I./proto/core --python_out=pkg/proto/core --grpc_python_out=pkg/proto/core ./proto/core/error.proto
 
-python -m grpc_tools.protoc -I./proto/user --python_out=pkg/proto/user --grpc_python_out=pkg/proto/user ./proto/user/user-microservice.proto
+python -m grpc_tools.protoc -I./proto/user --python_out=./pkg/proto/user --grpc_python_out=./pkg/proto/user ./proto/user/user-microservice.proto
 ```
--- pkg/proto/user/user_microservice_pb2.py
+++ pkg/proto/user/user_microservice_pb2.py
@@ -15,7 +15,7 @@ _sym_db = _symbol_database.Default()
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x17user-microservice.proto\x12\x04user\x1a\x1bgoogle/protobuf/empty.proto\"<\n\nNewUserReq\x12\r\n\x05login\x18\x01 \x01(\t\x12\r\n\x05\x65mail\x18\x02 \x01(\t\x12\x10\n\x08password\x18\x03 \x01(\t\";\n\rUpdateUserReq\x12\r\n\x05login\x18\x01 \x01(\t\x12\r\n\x05\x65mail\x18\x02 \x01(\t\x12\x0c\n\x04role\x18\x03 \x01(\x05\"\x17\n\tUserReqID\x12\n\n\x02id\x18\x01 \x01(\x05\"\x1b\n\x0bUserReqUuid\x12\x0c\n\x04uuid\x18\x01 \x01(\t\"\x1d\n\x0cUserReqLogin\x12\r\n\x05login\x18\x01 \x01(\t\"-\n\x0cSelectionReq\x12\r\n\x05limit\x18\x01 \x01(\x05\x12\x0e\n\x06offset\x18\x02 \x01(\x05\"t\n\x04User\x12\n\n\x02id\x18\x01 \x01(\x05\x12\x0c\n\x04uuid\x18\x02 \x01(\t\x12\r\n\x05login\x18\x03 \x01(\t\x12\r\n\x05\x65mail\x18\x04 \x01(\t\x12\x0c\n\x04role\x18\x05 \x01(\x05\x12\x12\n\ncreated_at\x18\x06 \x01(\t\x12\x12\n\nupdated_at\x18\x07 \x01(\t\"f\n\tSelection\x12\r\n\x05limit\x18\x01 \x01(\x05\x12\x0e\n\x06offset\x18\x02 \x01(\x05\x12\x18\n\x04\x64\x61ta\x18\x03 \x03(\x0b\x32\n.user.User\x12\r\n\x05total\x18\x04 \x01(\x05\x12\x11\n\tlast_page\x18\x05 \x01(\x05\x32\x94\x03\n\x0bUserService\x12*\n\nCreateUser\x12\x10.user.NewUserReq\x1a\n.user.User\x12*\n\x0bGetUserById\x12\x0f.user.UserReqID\x1a\n.user.User\x12.\n\rGetUserByUuid\x12\x11.user.UserReqUuid\x1a\n.user.User\x12\x30\n\x0eGetUserByLogin\x12\x12.user.UserReqLogin\x1a\n.user.User\x12+\n\nDeleteUser\x12\x11.user.UserReqUuid\x1a\n.user.User\x12-\n\nUpdateUser\x12\x13.user.UpdateUserReq\x1a\n.user.User\x12-\n\x06GetAll\x12\x12.user.SelectionReq\x1a\x0f.user.Selection\x12@\n\x0eHeartbeatCheck\x12\x16.google.protobuf.Empty\x1a\x16.google.protobuf.EmptyB(Z&github.com/wrs-news/node-protocol/userb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x17user-microservice.proto\x12\x04user\x1a\x1bgoogle/protobuf/empty.proto\"<\n\nNewUserReq\x12\r\n\x05login\x18\x01 \x01(\t\x12\r\n\x05\x65mail\x18\x02 \x01(\t\x12\x10\n\x08password\x18\x03 \x01(\t\"I\n\rUpdateUserReq\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12\r\n\x05login\x18\x02 \x01(\t\x12\r\n\x05\x65mail\x18\x03 \x01(\t\x12\x0c\n\x04role\x18\x04 \x01(\x05\"\x17\n\tUserReqID\x12\n\n\x02id\x18\x01 \x01(\x05\"\x1b\n\x0bUserReqUuid\x12\x0c\n\x04uuid\x18\x01 \x01(\t\"\x1d\n\x0cUserReqLogin\x12\r\n\x05login\x18\x01 \x01(\t\"-\n\x0cSelectionReq\x12\r\n\x05limit\x18\x01 \x01(\x05\x12\x0e\n\x06offset\x18\x02 \x01(\x05\"t\n\x04User\x12\n\n\x02id\x18\x01 \x01(\x05\x12\x0c\n\x04uuid\x18\x02 \x01(\t\x12\r\n\x05login\x18\x03 \x01(\t\x12\r\n\x05\x65mail\x18\x04 \x01(\t\x12\x0c\n\x04role\x18\x05 \x01(\x05\x12\x12\n\ncreated_at\x18\x06 \x01(\t\x12\x12\n\nupdated_at\x18\x07 \x01(\t\"f\n\tSelection\x12\r\n\x05limit\x18\x01 \x01(\x05\x12\x0e\n\x06offset\x18\x02 \x01(\x05\x12\x18\n\x04\x64\x61ta\x18\x03 \x03(\x0b\x32\n.user.User\x12\r\n\x05total\x18\x04 \x01(\x05\x12\x11\n\tlast_page\x18\x05 \x01(\x05\x32\x94\x03\n\x0bUserService\x12*\n\nCreateUser\x12\x10.user.NewUserReq\x1a\n.user.User\x12*\n\x0bGetUserById\x12\x0f.user.UserReqID\x1a\n.user.User\x12.\n\rGetUserByUuid\x12\x11.user.UserReqUuid\x1a\n.user.User\x12\x30\n\x0eGetUserByLogin\x12\x12.user.UserReqLogin\x1a\n.user.User\x12+\n\nDeleteUser\x12\x11.user.UserReqUuid\x1a\n.user.User\x12-\n\nUpdateUser\x12\x13.user.UpdateUserReq\x1a\n.user.User\x12-\n\x06GetAll\x12\x12.user.SelectionReq\x1a\x0f.user.Selection\x12@\n\x0eHeartbeatCheck\x12\x16.google.protobuf.Empty\x1a\x16.google.protobuf.EmptyB(Z&github.com/wrs-news/node-protocol/userb\x06proto3')
 
 
 
@@ -91,19 +91,19 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _NEWUSERREQ._serialized_start=62
   _NEWUSERREQ._serialized_end=122
   _UPDATEUSERREQ._serialized_start=124
-  _UPDATEUSERREQ._serialized_end=183
-  _USERREQID._serialized_start=185
-  _USERREQID._serialized_end=208
-  _USERREQUUID._serialized_start=210
-  _USERREQUUID._serialized_end=237
-  _USERREQLOGIN._serialized_start=239
-  _USERREQLOGIN._serialized_end=268
-  _SELECTIONREQ._serialized_start=270
-  _SELECTIONREQ._serialized_end=315
-  _USER._serialized_start=317
-  _USER._serialized_end=433
-  _SELECTION._serialized_start=435
-  _SELECTION._serialized_end=537
-  _USERSERVICE._serialized_start=540
-  _USERSERVICE._serialized_end=944
+  _UPDATEUSERREQ._serialized_end=197
+  _USERREQID._serialized_start=199
+  _USERREQID._serialized_end=222
+  _USERREQUUID._serialized_start=224
+  _USERREQUUID._serialized_end=251
+  _USERREQLOGIN._serialized_start=253
+  _USERREQLOGIN._serialized_end=282
+  _SELECTIONREQ._serialized_start=284
+  _SELECTIONREQ._serialized_end=329
+  _USER._serialized_start=331
+  _USER._serialized_end=447
+  _SELECTION._serialized_start=449
+  _SELECTION._serialized_end=551
+  _USERSERVICE._serialized_start=554
+  _USERSERVICE._serialized_end=958
 # @@protoc_insertion_point(module_scope)
